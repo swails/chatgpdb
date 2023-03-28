76450418782408f4a8c38678a08d0420ea598fe5
Jason Swails <jason.swails@gmail.com>
2023-03-27 22:03:31 -0400

    Add css file

--- chatgpdb/settings.py
+++ chatgpdb/settings.py
@@ -104,6 +104,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
